2ebebaab81f5a228382a7b4365289a7fa22a891c
Wouter Deman <wouter.deman@telenet.be>
2014-02-21 21:35:18 +0100

    Fixed watch (express) and removed console.log statements

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -9,7 +9,7 @@ var initialized = false;
 var saveToken = function saveToken(token) {
 	chrome.storage.sync.set({
 		'token': token
-	}, function() {
+	}, function () {
 		console.log("token set");
 	});
 }
@@ -17,14 +17,14 @@ var saveToken = function saveToken(token) {
 var getLastToken = function getLastToken() {
 	var dfd = new $.Deferred();
 
-	chrome.storage.sync.get('token', function(result) {
+	chrome.storage.sync.get('token', function (result) {
 		dfd.resolve(result.token);
 	});
 
 	return dfd;
 };
 
-var authenticateUser = function() {
+var authenticateUser = function () {
 	var dfd = new $.Deferred();
 
 	var redirectUrl = 'https://' + chrome.runtime.id + '.chromiumapp.org/';
@@ -44,7 +44,7 @@ var authenticateUser = function() {
 		'url': authUrl
 	};
 	chrome.identity.launchWebAuthFlow(options,
-		function(responseUrl) {
+		function (responseUrl) {
 			console.log('launchWebAuthFlow completed', chrome.runtime.lastError,
 				redirectUrl);
 
@@ -65,23 +65,24 @@ var authenticateUser = function() {
 				grant_type: "authorization_code",
 			};
 
-			$.post(tokenurl, data).done(function(res) {
-				var requestTokenUrl = "http://timesheetservice.herokuapp.com/auth/token"; // "http://localhost:3000/auth/token";
-				var tokenRequestData = {					
+			$.post(tokenurl, data).done(function (res) {
+				//var requestTokenUrl = "http://timesheetservice.herokuapp.com/auth/token"; 
+				var requestTokenUrl = "http://localhost:3000/auth/token";
+				var tokenRequestData = {
 					refreshtoken: res.refresh_token,
 					provider: "google"
 				};
 
-				$.post(requestTokenUrl, tokenRequestData).done(function(res) {
+				$.post(requestTokenUrl, tokenRequestData).done(function (res) {
 					console.table("POST SUCCES", res, data);
 					isAuthenticating = false;
 					saveToken(res);
 					dfd.resolve(res);
-				}).fail(function(a, b, c, d) {
+				}).fail(function (a, b, c, d) {
 					console.log("POST failed", a, b, c, d);
 					dfd.fail();
 				});
-			}).fail(function(a, b, c, d) {
+			}).fail(function (a, b, c, d) {
 				console.log("POST failed", a, b, c, d);
 				dfd.fail();
 			});
@@ -90,7 +91,7 @@ var authenticateUser = function() {
 	return dfd;
 };
 
-var deparam = function(querystring) {
+var deparam = function (querystring) {
 	// remove any preceding url and split
 	querystring = querystring.substring(querystring.indexOf('?') + 1).split('&');
 	var params = {}, pair, d = decodeURIComponent,
@@ -104,25 +105,25 @@ var deparam = function(querystring) {
 	return params;
 }
 
-var getLocation = function() {
+var getLocation = function () {
 	var dfd = new $.Deferred();
-	navigator.geolocation.getCurrentPosition(function(position) {
+	navigator.geolocation.getCurrentPosition(function (position) {
 		dfd.resolve(position.coords);
-	}, function(err) {
+	}, function (err) {
 		dfd.reject(err);
 		console.log("Error getting current position", err);
 	});
 	return dfd;
 }
 
-var postGeoLocation = function() {
+var postGeoLocation = function () {
 	var dfd = new $.Deferred();
 
-	getLastToken().then(function(token) {
-		getLocation().then(function(coords) {
+	getLastToken().then(function (token) {
+		getLocation().then(function (coords) {
 			var loc = [coords.latitude, coords.longitude];
-			var url = "http://timesheetservice.herokuapp.com/entry";
-			//var url="http://localhost:3000/entry";
+			//var url = "http://timesheetservice.herokuapp.com/entry";
+			var url = "http://localhost:3000/entry";
 			var data = {
 				type: 'locationinfo',
 				userinfo: {
@@ -133,37 +134,36 @@ var postGeoLocation = function() {
 				token: token
 			};
 
-			$.post(url, data).done(function(d) {
+			$.post(url, data).done(function (d) {
 				console.table("POST SUCCES", d, data);
-			}).fail(function(a, b, c, d) {
+			}).fail(function (a, b, c, d) {
 				dfd.reject();
 			});
 		});
 
-	}).fail(function() {
+	}).fail(function () {
 		dfd.reject();
 	});
 
 	return dfd;
 };
 
-var run = function() {
-	return;
+var run = function () {
 	if (!initialized && !isAuthenticating) {
 		init();
 		return false;
-	} else if(!isAuthenticating && initialized) {
-		postGeoLocation().fail(function() {
+	} else if (!isAuthenticating && initialized) {
+		postGeoLocation().fail(function () {
 			init();
-		});		
+		});
 	}
-	return true;	
+	return true;
 };
 
-var init = function() {
-	if(!lastTokenTryOut) {
-		getLastToken().then(function(token) {
-			if(!token) {
+var init = function () {
+	if (!lastTokenTryOut) {
+		getLastToken().then(function (token) {
+			if (!token) {
 				authenticateUser();
 			}
 			lastTokenTryOut = true;
@@ -184,11 +184,11 @@ var alarmCallbacks = {};
 alarmCallbacks[alarmKey] = run;
 
 var alarm = chrome.alarms.create(alarmKey, alarmInfo);
-chrome.alarms.onAlarm.addListener(function(aInfo) {
+chrome.alarms.onAlarm.addListener(function (aInfo) {
 	alarmCallbacks[aInfo.name]();
 });
 
 var res = run();
-if(!res) {
+if (!res) {
 	run();
 }
