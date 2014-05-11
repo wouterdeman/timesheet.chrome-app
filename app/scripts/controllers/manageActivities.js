06c7a46003763df02c60264cff9a3697af721f0b
Tim Stuyckens <timstuyckens@gmail.com>
2014-05-11 13:25:56 +0200

    afwerken manage activities

--- app/scripts/controllers/manageActivities.js
+++ app/scripts/controllers/manageActivities.js
@@ -6,33 +6,42 @@ angular.module('timesheetApp')
 		    "July", "August", "September", "October", "November", "December" ];
     	var today=new Date();
     	var year=today.getFullYear();
+    	var currentDevice=-1;
+
         $scope.years=[year-1,year,year+1];
         $scope.selectedYear=year;
         $scope.months = monthNames.map(function(i,e){
         					return {id:e, name:i};
         				});
         $scope.selectedMonth=$scope.months[today.getMonth()];
+        $scope.customers=[];
+        $scope.trackedTimes=[];
+         $scope.devices=[];
+         $scope.selectedDevice;
+
+         $scope.trackedTimesForDevice=function(){
+         	var deviceId= $scope.selectedDevice;
+         	return _.filter( $scope.trackedTimes,{device:deviceId});
+         };
 
 
-        $scope.trackedTime=[];
-/*
-customers/trackedTimeAndCustomer
-token: req.body.token,
- 	 month: req.body.month,
- 	 year: req.body.year
-To list all available commands enter "/?".
-	
-Wouter Deman	3:16 PM
-/customers/updateCustomerForTrackedTime
-token: req.body.token,
- 	 day: req.body.day,
- 	 month: req.body.month,
- 	 year: req.body.year,
- 	 device: req.body.device,
- 	 customer: req.body.customer
-*/
+         //INIT
+        chromeApp.getLastToken().then(function (token) {
+            var getCustomerData = {
+                token: token
+            };
+
+            $http.post(urls.customers.all, getCustomerData).success(function (customers) {
+            	console.log("customers",customers);
+                $scope.customers = customers;
+            });
+        });
+        chromeApp.getClientHash().then(function(hash){
+			currentDevice=hash;
+        });
+
+		//ACTIONS
         $scope.getTrackedTime=function(){
-
 			chromeApp.getLastToken().then(function (token) {
 	            var getCustomerData = {
 	                token: token,
@@ -42,7 +51,39 @@ token: req.body.token,
 
 	            $http.post(urls.customers.trackedTimeAndCustomer, getCustomerData).success(function (trackedTime) {
 	                console.log("trackedTime",trackedTime);
+	                $scope.trackedTimes=trackedTime;
+
+	                var grouped=_.groupBy(trackedTime,'device');
+	               	var deviceIds=_.keys(grouped);
+	               	var devices=_.map(deviceIds,function(deviceId){
+	               		var name=_.first(grouped[deviceId]).devicedetails.devicetype;
+	               		if(deviceId===currentDevice)
+	               			name+=" (huidig)";
+	               		return {id: deviceId, name:name};
+	               	});
+
+	                $scope.devices=devices;
+	                $scope.selectedDevice=currentDevice;
 	            });
 	        });
         };
+
+
+        $scope.saveActivity = function(trackedTimeForDevice){
+        	var date=new Date(trackedTimeForDevice.date);
+			chromeApp.getLastToken().then(function (token) {
+	            var updateCustomerData = {
+	                token: token,
+					 day: date.getDate(),
+				 	 month: date.getMonth(),
+				 	 year: date.getFullYear(),
+				 	 device:  $scope.selectedDevice,
+				 	 customer: trackedTimeForDevice.customer
+	            };
+
+	            $http.post(urls.customers.updateCustomerForTrackedTime, updateCustomerData).success(function (response) {
+	                console.log("updated",response);
+	            });
+	        });
+        }
     });
