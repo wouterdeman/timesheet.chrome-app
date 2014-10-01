97ba054a25ce625d511db75f4cb3e11b6e63baca
Wouter Deman <wouter.deman@telenet.be>
2014-10-01 22:59:36 +0200

    Adjusted save absence for the new service features (client logic was moved to server)

--- app/scripts/controllers/absences.js
+++ app/scripts/controllers/absences.js
@@ -2,42 +2,38 @@
 
 angular.module('timesheetApp')
     .service('AbsenceService', ['$http', '$q', 'urls',
-        function ($http, $q, urls) {
+        function($http, $q, urls) {
             var index = urls.absences.index;
             var detail = urls.absences.detail;
             return {
-                getAll: function () {
+                getAll: function() {
                     var deferred = $q.defer();
-                    $http.get(index).success(function (absences) {
+                    $http.get(index).success(function(absences) {
                         deferred.resolve(absences);
-                    }).error(function (err) {
+                    }).error(function(err) {
                         deferred.reject(err);
                     });
                     return deferred.promise;
                 },
-                save: function (absences) {
-                    var promises = absences.map(function (absence) {
-                        return $http.post(index, absence);
-                    });
-
-                    return $q.all(promises);
+                save: function(absence) {
+                    return $http.post(index, absence);
                 },
-                remove: function (absences) {
-                    var promises = absences.map(function (absence) {
+                remove: function(absences) {
+                    var promises = absences.map(function(absence) {
                         return $http.delete(detail + absence._id);
                     });
                     return $q.all(promises);
                 }
             };
         }
-    ]).controller('AbsencesController', function ($scope, $http, $location, $ionicLoading, AbsenceService, $ionicPopup, dateFilter, $q) {
+    ]).controller('AbsencesController', function($scope, $http, $location, $ionicLoading, AbsenceService, $ionicPopup, dateFilter, $q) {
         //todo: refactor loading stuff in decorator
         $ionicLoading.show({
             template: 'Loading...'
         });
-        $scope.doRefresh = function () {
-            AbsenceService.getAll().then(function (absences) {
-                absences = _.sortBy(absences, function (absence) {
+        $scope.doRefresh = function() {
+            AbsenceService.getAll().then(function(absences) {
+                absences = _.sortBy(absences, function(absence) {
                     var t = new Date(absence.date);
                     return t.getTime();
                 });
@@ -46,7 +42,7 @@ angular.module('timesheetApp')
                 var lastAbsence;
                 var group = {};
 
-                var initGroup = function (absence) {
+                var initGroup = function(absence) {
                     lastAbsence = absence;
                     group = {};
                     group.startdate = absence.date;
@@ -58,7 +54,7 @@ angular.module('timesheetApp')
                     groups.push(group);
                 };
 
-                _.forEach(absences, function (absence) {
+                _.forEach(absences, function(absence) {
                     if (!lastAbsence) {
                         initGroup(absence);
                     } else {
@@ -74,33 +70,33 @@ angular.module('timesheetApp')
                     }
                 });
 
-                groups = _.map(groups, function (group) {
+                groups = _.map(groups, function(group) {
                     group.startdateFormatted = dateFilter(group.startdate, 'EEE dd/MM/yy');
                     group.enddateFormatted = dateFilter(group.enddate, 'EEE dd/MM/yy');
                     return group;
                 });
 
-                groups = _.sortBy(groups, function (group) {
+                groups = _.sortBy(groups, function(group) {
                     var t = new Date(group.startdate);
                     return -t.getTime();
                 });
 
                 var today = moment().startOf('day');
 
-                var futureGroups = _.filter(groups, function (group) {
+                var futureGroups = _.filter(groups, function(group) {
                     return new Date(group.startdate) >= today || new Date(group.enddate) >= today;
                 });
 
-                var pastGroups = _.filter(groups, function (group) {
+                var pastGroups = _.filter(groups, function(group) {
                     return new Date(group.startdate) < today && new Date(group.enddate) && today;
                 });
 
                 $scope.futureGroups = futureGroups;
                 $scope.pastGroups = pastGroups;
                 $ionicLoading.hide();
-            }).catch(function () {
+            }).catch(function() {
                 $ionicLoading.hide();
-            }).finally(function () {
+            }).finally(function() {
                 // Stop the ion-refresher from spinning
                 $scope.$broadcast('scroll.refreshComplete');
             });
@@ -108,20 +104,20 @@ angular.module('timesheetApp')
 
         $scope.doRefresh();
 
-        $scope.remove = function (absences) {
+        $scope.remove = function(absences) {
             var confirmPopup = $ionicPopup.confirm({
                 title: 'Delete',
                 template: 'Are you sure you want to delete this absence?'
             });
-            confirmPopup.then(function (res) {
+            confirmPopup.then(function(res) {
                 if (res) {
-                    AbsenceService.remove(absences).then(function () {
+                    AbsenceService.remove(absences).then(function() {
                         $scope.doRefresh();
                     });
                 }
             });
         };
-    }).controller('AbsencesDetailController', function ($stateParams, $scope, AbsenceService, dateFilter, $state, $ionicPopup) {
+    }).controller('AbsencesDetailController', function($stateParams, $scope, AbsenceService, dateFilter, $state, $ionicPopup) {
         $scope.absence = {
             from: dateFilter(new Date(), 'yyyy-MM-dd'),
             to: dateFilter(new Date(), 'yyyy-MM-dd'),
@@ -129,45 +125,38 @@ angular.module('timesheetApp')
             prenoon: "true"
         };
 
-        $scope.save = function (valid) {
+        $scope.save = function(valid) {
             $scope.submitted = true;
             if (!valid) {
                 return;
             }
 
             var absence = $scope.absence;
-            var data = [];
-            for (var d = new Date(absence.from); d <= new Date(absence.to); d.setDate(d.getDate() + 1)) {
-                var dayOfWeek = d.getDay();
-                if (dayOfWeek !== 0 && dayOfWeek !== 6) {
-                    var absenceToSave = {
-                        year: dateFilter(d, 'yyyy'),
-                        month: dateFilter(d, 'MM'),
-                        day: dateFilter(d, 'dd'),
-                        amount: 1
-                    };
+            var absenceToSave = {
+                fromYear: dateFilter(absence.from, 'yyyy'),
+                fromMonth: dateFilter(absence.from, 'MM'),
+                fromDay: dateFilter(absence.from, 'dd'),
+                toYear: dateFilter(absence.to, 'yyyy'),
+                toMonth: dateFilter(absence.to, 'MM'),
+                toDay: dateFilter(absence.to, 'dd'),
+                amount: 1
+            };
 
-                    if (absence.halfday) {
-                        absenceToSave.amount = 0.5;
-                        absenceToSave.prenoon = absence.prenoon;
-                    }
-
-                    data.push(absenceToSave);
-                }
+            if (absence.halfday) {
+                absenceToSave.amount = 0.5;
+                absenceToSave.prenoon = absence.prenoon;
             }
 
-            AbsenceService.save(data).then(function (results) {
+            AbsenceService.save(absenceToSave).then(function(result) {
                 var success = true;
 
-                _.forEach(results, function (result) {
-                    if (!result.data.success) {
-                        success = false;
-                        $ionicPopup.alert({
-                            title: 'Absence registration failed',
-                            template: result.data.message
-                        });
-                    }
-                });
+                if (!result.data.success) {
+                    success = false;
+                    $ionicPopup.alert({
+                        title: 'Absence registration failed',
+                        template: result.data.message
+                    });
+                }
 
                 if (success) {
                     $state.go('gretel.absences.list');
