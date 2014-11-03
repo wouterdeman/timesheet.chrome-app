333bc5db26b01dea72bce271c8109733ada0a3b6
Wouter Deman <wouter.deman@telenet.be>
2014-11-03 22:46:59 +0100

    Added place detail information modal

--- app/gretel/places/places.controller.js
+++ app/gretel/places/places.controller.js
@@ -1,10 +1,24 @@
 'use strict';
 
 angular.module('timesheetApp')
-    .controller('PlacesController', function ($scope, $http, $location) {
+    .controller('PlacesController', function ($scope, $http, $location, $ionicModal) {
+        $scope.showPlace = showPlace;
+        $scope.doRefresh = doRefresh;
         activate();
 
         function activate() {
+
+
+            $ionicModal.fromTemplateUrl('gretel/places/place.modal.html', {
+                scope: $scope
+            }).then(function (modal) {
+                $scope.modal = modal;
+            });
+
+            doRefresh();
+        }
+
+        function doRefresh() {
             chromeApp.getLocation().then(function (coords) {
                 $scope.places = [];
 
@@ -12,7 +26,11 @@ angular.module('timesheetApp')
                     if (data.meta && data.meta.code === 200) {
                         var places = _.map(data.response.venues, function (venue) {
                             var place = {
-                                name: venue.name + ' (FSQ)'
+                                name: venue.name + ' (FSQ)',
+                                originalname: venue.name,
+                                categories: [],
+                                url: venue.url,
+                                contact: venue.contact
                             };
 
                             if (venue.location) {
@@ -21,6 +39,7 @@ angular.module('timesheetApp')
 
                             if (venue.categories && venue.categories.length > 0) {
                                 var category = venue.categories[0];
+
                                 if (category.icon && category.icon.prefix) {
                                     place.icon = category.icon.prefix + 'bg_32' + category.icon.suffix;
 
@@ -37,11 +56,15 @@ angular.module('timesheetApp')
                                 }
                             }
 
+                            _.forEach(venue.categories, function (category) {
+                                place.categories.push(category.name);
+                            });
+
                             $scope.places.push(place);
                             return place;
                         });
+                        console.log(data);
                     }
-                    console.log(data);
                 });
 
                 $http.get("https://maps.googleapis.com/maps/api/place/search/json?location=" + coords.latitude + "," + coords.longitude + "&radius=100&sensor=true&key=AIzaSyBrxt1UXQXNNfpPAkBJBLKeUj9XN2tZXlo").success(function (data) {
@@ -49,17 +72,40 @@ angular.module('timesheetApp')
                         var places = _.map(data.results, function (result) {
                             var place = {
                                 name: result.name + ' (G)',
+                                originalname: result.name,
                                 address: result.vicinity,
-                                icon: result.icon
+                                icon: result.icon,
+                                categories: result.types,
+                                googledetail: result.place_id
                             };
 
                             $scope.places.push(place);
 
                             return place;
                         });
+                        console.log(data);
                     }
-                    console.log(data);
                 });
+                // Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
             });
-        }
+        };
+
+        function showPlace(place) {
+            $scope.place = place;
+
+            if (place.googledetail) {
+                $http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=" + place.googledetail + "&key=AIzaSyBrxt1UXQXNNfpPAkBJBLKeUj9XN2tZXlo").success(function (data) {
+                    if (data.status === "OK") {
+                        place.url = data.result.website || data.result.url;
+                        place.contact = {
+                            formattedPhone: data.result.international_phone_number,
+                            phone: data.result.international_phone_number
+                        };
+                    }
+                });
+            }
+
+            $scope.modal.show();
+        };
     });
